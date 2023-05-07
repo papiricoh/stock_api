49aaf24300f115e8d5b15d6e84bc9f32b8652371
Papiricoh <slendeman0200@gmail.com>
2023-05-07 02:44:10 +0200

    added comments of parameters in index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -5,9 +5,9 @@ const userController = require('../controllers/userController');
 router.get('/users/:id', userController.getCheckUser);
 router.get('/company/:label', userController.getCompanyData);
 router.get('/companies', userController.getCompaniesList);
-router.post('/newcompany', userController.postCreateCompany);
-router.post('/shares/buy', userController.postBuyShares);
-router.post('/user/deposit', userController.postDepositMoney);
-router.post('/user/withdraw', userController.postWithdrawMoney);
+router.post('/newcompany', userController.postCreateCompany); //name, label, owner_id, total_shares, initial_money, percentage_sold
+router.post('/shares/buy', userController.postBuyShares); //company_label, player_id, quantity,
+router.post('/user/deposit', userController.postDepositMoney); //player_id, deposit
+router.post('/user/withdraw', userController.postWithdrawMoney); //player_id, withdraw
 
 module.exports = router;
